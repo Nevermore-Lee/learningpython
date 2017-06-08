02fc8796a157c48e6f02fb4561f9c59ac681754f
Lee <lihaosyd@gmail.com>
2017-06-08 18:10:26 +0800

    Initial commit

---
+++ udp_s_test.py
@@ -0,0 +1,10 @@
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('127.0.0.1',9999))
+print('Bind UDP on 9999...')
+while True:
+    #接受数据
+    data,addr = s.recvfrom(1024)
+    print(data,addr)
+    print('Received from %s:%s'%addr)
+    s.sendto(b'Hello,%s!'%data,addr)
